83d5b29bf9b79487fa2d9a5976fb26d18e2b5ea2
sdtareq <sdtareq@gmail.com>
2017-10-22 01:26:25 +0600

    welcome note added

--- app/src/main/java/com/example/tareq/simpleapp/MainActivity.kt
+++ app/src/main/java/com/example/tareq/simpleapp/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.tareq.simpleapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Toast.makeText(this, "Hello World", Toast.LENGTH_LONG).show()
     }
 }
